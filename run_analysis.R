1ea1efef6c94fa92a580e5328f73a97875de4253
Nancyl <dhao527@gmail.com>
2015-03-21 16:26:12 -0400

    add CodeBook.md README.md run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,3 @@
-
-## This is a markdown file
-
-*1.Download test set and train set data, combine two of them as m
-
 url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download. 
 X_test<-download.file(url, destfile="./UCI HAR Dataset/test/X_test", method="curl")
@@ -11,24 +6,20 @@ dim(xtest)
  X_train<-download.file(url, destfile="./UCI HAR Dataset/train/X_train", method="curl")
 xtrain<-read.table("./UCI HAR Dataset/train/X_train.txt")
 dim(xtrain)
-m<-rbind(xtest,xtrain)
-dim(m)
-names(m)
-
-*4. Add 561 features as variable names
-install.packages("reshape2")
-library(tidyr)
-library(dplyr)
 var<-download.file(url, destfile="./UCI HAR Dataset/features.txt", method="curl")
 varnames<-read.table("./UCI HAR Dataset/features.txt")
 dim(varnames)
+m<-rbind(xtest,xtrain)
+dim(m)
+names(m)
+install.packages("reshape2")
+library(tidyr)
+library(dplyr)
 colnames(m) <- varnames$V2
 head(m,1)
 
-* 2.Extract only the measurements of mean and standard deviation from m and generate m1
  m1<-m[,grepl("mean|std", colnames(m))]
 
-* 3.Add subject and activity label to the previous dataset m1 
 y_test<-download.file(url, destfile="./UCI HAR Dataset/test/y_test", method="curl")
 ytest<-read.table("./UCI HAR Dataset/test/y_test.txt")
 str(ytest)
@@ -39,8 +30,6 @@ act<-rbind(ytest,ytrain)
 m2<-cbind(act,m1)
 m2<-rename(m2, ACT=V1)
 head(m2,1)
-
-*5. Generate a tidy dataset with the mean of each variables of each subject and each activity.
 
 subject_test<-download.file(url, destfile="./UCI HAR Dataset/test/subject_test", method="curl")
 stest<-read.table("./UCI HAR Dataset/test/subject_test.txt")
@@ -54,8 +43,7 @@ dim(m3)
 
 by_SUBJECT<-group_by(m3,SUBJECT,ACT)
 m4<-summarise_each(by_SUBJECT,funs(mean))
-
-* Write the new dataset as a text file "activity" in local repo.
 write.table(m4, file = "activityfinal.txt",row.names=FALSE)
+
 
 
